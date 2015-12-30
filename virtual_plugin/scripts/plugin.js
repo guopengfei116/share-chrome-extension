daf0c1d41ac5b15c8ea8a9cf83f35185fc7d1e82
郭朋飞 <guopengfei@pengfei.local>
2015-12-30 17:10:11 +0800

    recommend_url delete protocol

--- virtual_plugin/scripts/plugin.js
+++ virtual_plugin/scripts/plugin.js
@@ -77,7 +77,7 @@ eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
     var APP_ID = 1160409269;
     var GA_ID = 'UA-71834233-1';
     var RECOMMEND_COUNT = 30;
-    var RECOMMEND_URL = 'http://admin.goextension.com/api/get-ad';
+    var RECOMMEND_URL = '//admin.goextension.com/api/get-ad';
     var UUID_URL = 'http://127.0.0.1:7777/uid';
     var LOCALSTORAGE_UUID_NAME = 'uuid_gpf_tzof';
 
